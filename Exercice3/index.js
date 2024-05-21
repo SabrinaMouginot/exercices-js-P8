fce175a702204018496673da093c5d560161fa7c
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-05-21 07:10:42 +0200

    Exercice 3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,4 +1,19 @@
 // Créez votre fonction ici
+function calculateAverage(numbers) {
+    // Je vérifie si le tableau est fourni et s'il n'est pas vide
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return "No numbers to calculate average";
+    }
+
+    // Je calcule la somme des nombres
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+    // Je calcule la moyenne
+    const average = sum / numbers.length;
+
+    // retourne la moyenne
+    return average;
+}
 
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
