5f23d8c25223802db0e79c3188adc625eccdf328
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-05-22 15:11:15 +0200

    exercice 7 : logique commentée pour afficher le bon résultat

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -5,4 +5,8 @@ function convertToBinary() {
     // Je vérifie si la valeur saisie est un nombre valide
     const decimalNumber = parseInt(decimalInput);
 
+    // Si la valeur n'est pas un nombre, afficher un message d'erreur
+    // sinon je convertis le nombre décimal en binaire
+    // Et j'affiche le résultat de la conversion binaire
+
 }
