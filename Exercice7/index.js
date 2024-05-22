922ffa35900a11bb70ec828c8db51294eee5a969
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-05-22 15:07:40 +0200

    exercice 7 : creation de la fonction convertToBinary ()

---
+++ Exercice7/index.js
@@ -0,0 +1,8 @@
+function convertToBinary() {
+    // Je vais chercher l'id de la valeur du champ de saisie
+    const decimalInput = document.getElementById('decimalInput').value;
+
+    // Je vérifie si la valeur saisie est un nombre valide
+    const decimalNumber = parseInt(decimalInput);
+
+}
