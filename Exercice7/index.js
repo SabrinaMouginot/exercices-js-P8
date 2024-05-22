d3e1ff5bbde1994776fac7511018bb548e0522f7
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-05-22 15:37:44 +0200

    exercice 7 : verification si la valeur saisie est valide et son affichage

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -8,5 +8,15 @@ function convertToBinary() {
     // Si la valeur n'est pas un nombre, afficher un message d'erreur
     // sinon je convertis le nombre décimal en binaire
     // Et j'affiche le résultat de la conversion binaire
+    
+    if (isNaN(decimalNumber)) {
+        // Si la valeur n'est pas un nombre, afficher un message d'erreur
+        document.getElementById('binaryResult').textContent = "Veuillez entrer un nombre entier valide.";
+    } else {
+        // Convertir le nombre décimal en binaire
+        const binaryResult = decimalNumber.toString(2);
 
+        // Afficher le résultat de la conversion binaire
+        document.getElementById('binaryResult').textContent = `Représentation binaire : ${binaryResult}`;
+    }
 }
