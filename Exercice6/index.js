e73d970dad71b5eaf031edb6dc45ad4e06c7fc7f
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-05-23 12:37:05 +0200

    P8 exo 6 simplification du code

--- Exercice6/index.js
+++ Exercice6/index.js
@@ -1,18 +1,16 @@
+const display = document.getElementById('display');
 // Ajouter des chiffres et des opérateurs à l'affichage
 function appendToDisplay(value) {
-    const display = document.getElementById('display');
     display.value += value;
 }
 
 // Effacer l'affichage
 function clearDisplay() {
-    const display = document.getElementById('display');
     display.value = '';
 }
 
 // Calcul et affichage le résultat
 function calculateResult() {
-    const display = document.getElementById('display');
     try {
         // Utiliser eval pour calculer le résultat
         const result = eval(display.value);
